8453f1a9a323863f4a8e166727bebc51f8ec63f0
Edward <k3s47@outlook.com>
2018-11-12 14:01:31 +0700

    Footer not Complete 80%
    Most part have done not finish footer

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,33 +1,75 @@
 // Main
-$(document).ready(function () {
-	// Phần Slider
-	$('.home-slider .owl-carousel').owlCarousel({
-		items: 1,
-		nav: false,
-		dots: true,
-		navText: ['<i class="fa fa-chevron-left"></i>', '<i class="fa fa-chevron-right"></i>'],
-	});
-	// Phần Clients
-	$('.home-clients .owl-carousel').owlCarousel({
-		loop:true,
-		responsiveClass:true,
-		items: 1,
-		nav: true,
-		dots: false,
-		navText: ['<i class="fa fa-chevron-left"></i>', '<i class="fa fa-chevron-right"></i>'],
-		responsive: {
-			// breakpoint from 480 up
-			480: {
-				items: 2,
-			},
-			// breakpoint from 768 up
-			768: {
-				items: 4,
-			},
-			// breakpoint from 992 up
-			992: {
-				items: 6,
-			}
-		}
-	});
-});
+console.log('FullStack Vietnam!')
+$('.owl-one').owlCarousel({
+    loop:true,
+    margin:0,
+	nav:true,
+	responsiveClass:true,
+	dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        768:{
+			items:1,
+			dots: true,
+			nav:false
+        },
+        1000:{
+			items:1,
+			nav:false,
+			dots: true,
+        }
+    }
+})
+
+$('.owl-two').owlCarousel({
+    loop:true,
+    margin:0,
+	nav:true,
+	responsiveClass:true,
+	dots:false,
+	center: true,
+	items:3,
+	
+    responsive:{
+        0:{
+            items:1
+        },
+        768:{
+			items:1.5,
+			nav: true,
+			dots:false
+        },
+        1000:{
+			items:1.5,
+			dots:false,
+			nav: true,
+        }
+    }
+})
+
+
+$('.owl-three').owlCarousel({
+    loop:true,
+    margin:0,
+	nav:true,
+	responsiveClass:true,
+	dots:false,
+	
+	items:1,
+	
+    responsive:{
+        0:{
+            items:1
+        },
+        576:{
+			items:2,
+
+			
+		},
+        992:{
+			items:4,
+        }
+    }
+})
